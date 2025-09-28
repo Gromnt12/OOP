06c6c63519dda39bc30fdb3bdabe1a3b3340b41e
Gromnt <login9816@mail.ru>
2025-09-29 04:33:57 +0700

    Task_1_1_1

---
+++ Task_1_1_1/src/main/java/ru/nsu/bukhanov/MAIN.java
@@ -0,0 +1,16 @@
+package ru.nsu.bukhanov;
+
+import java.util.Arrays;
+
+public class MAIN {
+    /**
+     * Основной метод приложения
+     * @param args аргументы командной строки (не используются)
+     */
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 7, 9, 2};
+        System.out.println("Input array: " + Arrays.toString(arr));
+        int[] sortedArray = HeapSort.heapsort(arr);
+        System.out.println("Sorted array: " + Arrays.toString(sortedArray));
+    }
+}
